b3669721a6a9e334d0e946f43b9e2cfc11c1ab0e
Stéphanie Bellini <stephybellini@orange.fr>
2024-02-16 16:26:14 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+    <p class="text-4xl text-blue-700 text-center py-5">Hello la Mine!</p>
+    <p class="text-4xl text-red-700 text-center py-5">Hello la Meuh!</p>
+    </div>
+  );
+}
+
+export default App;
